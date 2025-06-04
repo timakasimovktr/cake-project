be695d21280134eb94ec8596153a927c4f6843f2
Timur Kasimov <106226856+timakasimovktr@users.noreply.github.com>
2025-06-04 22:37:47 +0500

    update

--- src/components/Forms/RegisterComponent.jsx
+++ src/components/Forms/RegisterComponent.jsx
@@ -86,7 +86,7 @@ export default function RegisterComponent() {
       );
 
       toast.success("Ro'yxatdan muvaffaqiyatli o'tdingiz!");
-      router.push("/dashboard");
+      router.push("/");
     } catch (error) {
       if (axios.isAxiosError(error)) {
         toast.error(error.response?.data?.message || "Xatolik kodda");
