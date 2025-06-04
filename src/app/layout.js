6e464941c485bab61c7d55f87dcf5b12a9dd561e
Timur Kasimov <106226856+timakasimovktr@users.noreply.github.com>
2025-06-04 18:50:24 +0500

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1397735421376354');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=1397735421376354&ev=PageView&noscript=1"
+        /></noscript>
+      </head>
       <body className={`${montserrat.variable} antialiased`}>
         <div className="stars"></div>
         <div className="twinkling"></div>
